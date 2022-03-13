bdbc8a76f6493b60a04c50c95487f17fd896fca4
vanribeiro <vanribeiro.dev@gmail.com>
2022-03-13 14:28:19 -0300

    fix page/api/my-instagram.js

--- page/api/my-instagram.js
+++ page/api/my-instagram.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
-
+dotenv.config();
 
 const myInstagramHandler = (res, req) =>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    
+
     const token = process.env.INSTRAGRAM_TOKEN_API;
     const media = 'id,caption,media_type,permalink,thumbnail_url,username,media_url';
     fetch(`https://graph.instagram.com/me/media?fields=${media}&access_token=${token}`)
