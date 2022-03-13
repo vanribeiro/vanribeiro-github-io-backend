c55cccc6d694dd8c8280173105b282a989f33d67
vanribeiro <vanribeiro.dev@gmail.com>
2022-03-13 16:27:55 -0300

    fix headers.

--- api/coursing-in-alura.js
+++ api/coursing-in-alura.js
@@ -11,7 +11,11 @@ router.get('/', (req, res) => {
     const token = process.env.ALURA_TOKEN_API;
     fetch(`https://www.alura.com.br/api/dashboard/${token}`)
     .then(response => response.json())
-    .then(data => res.send(data));
+    .then(data => res.send(data.courseProgresses));
+
+    res.status(200);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     
 });
 
--- api/my-instagram-media.js
+++ api/my-instagram-media.js
@@ -10,10 +10,14 @@ router.get('/', (req, res) => {
     
     const token = process.env.INSTRAGRAM_TOKEN_API;
     const media = 'id,caption,media_type,permalink,thumbnail_url,username,media_url';
+    
     fetch(`https://graph.instagram.com/me/media?fields=${media}&access_token=${token}`)
     .then(response => response.json())
     .then(data => res.send(data));
-    
+
+    res.status(200);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 
